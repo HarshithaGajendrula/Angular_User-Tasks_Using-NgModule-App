ef4dc792798ea1d3ebc3d6179413a8c9b56a55a8
Harshitha <harshitha.gajendrula@riskhawk.net>
2024-06-13 21:47:29 +0530

    Tasks App

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,28 @@
+
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {type User} from './user.model'
+import { CardComponent } from "../shared/card/card.component";
+
+@Component({
+    selector: 'app-user',
+    standalone: true,
+    templateUrl: './user.component.html',
+    styleUrl: './user.component.css',
+    imports: [CardComponent]
+})
+export class UserComponent {
+//   @Input({required : true}) id !: string;
+//  @Input({required : true}) avatar !: string;
+//  @Input({required : true}) name !: string;
+@Input({required : true}) user!: User;
+@Input({required: true}) selected!: boolean;
+@Output() select = new EventEmitter<string>();
+
+ get imagePath(){
+  return 'assets/users/users/' + this.user.avatar;
+}
+
+ onSelectUser() {
+  this.select.emit(this.user.id);
+ }
+}
